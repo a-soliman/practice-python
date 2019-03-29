63dae4f6d009df540f0c74e309a3108ed168cbb6
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-03-29 09:14:05 -0700

    solved excersise 1

--- 01-character-input.py
+++ 01-character-input.py
@@ -11,3 +11,39 @@ Extras:
 Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. (Hint: order of operations exists in Python)
 Print out that many copies of the previous message on separate lines. (Hint: the string "\n is the same as pressing the ENTER button)
 '''
+
+import datetime
+
+
+def main():
+    name = get_name()
+    age = get_age()
+    copys = get_copys_num()
+    hundred_on = get_year_100(age)
+    return message(name, hundred_on, copys)
+
+
+def get_name():
+    return input("What's your name? :")
+
+
+def get_age():
+    return int(input("How old are you? :"))
+
+
+def get_copys_num():
+    return int(input("Provide copys num: "))
+
+
+def get_year_100(age):
+    current_year = datetime.datetime.now().year
+    return (100 - age) + current_year
+
+
+def message(name, hundred_on, copys):
+    message = f"{name}, you'll be a 100 years old on ${hundred_on}\n"
+    print(message*copys)
+    return
+
+
+main()
