241c071967dc819998093dcbdb6bf350e6c9e913
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-03-29 09:25:54 -0700

    odd or even messaging

---
+++ 02-odd-or-even.py
@@ -0,0 +1,12 @@
+'''
+Odd Or Even 
+input if types int equality comparison numbers mod
+Again, the exercise comes first (with a few extras if you want the extra challenge or want to spend more time), followed by a discussion. Enjoy!
+
+Exercise 2 (and Solution)
+Ask the user for a number. Depending on whether the number is even or odd, print out an appropriate message to the user. Hint: how does an even / odd number react differently when divided by 2?
+
+Extras:
+
+If the number is a multiple of 4, print out a different message.
+'''
