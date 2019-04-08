6b9f4e6aafe5042782704d3947f3032de6e6066d
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-08 10:27:46 -0700

    solved 5

--- 05-list_overlap.py
+++ 05-list_overlap.py
@@ -10,3 +10,15 @@ Extras:
 Randomly generate two lists to test this
 Write this in one line of Python (don’t worry if you can’t figure this out at this point - we’ll get to it soon)
 '''
+
+
+def list_overlap(l1, l2):
+    s1 = set(l1)
+    s2 = set(l2)
+    return list(s1.intersection(s2))
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+print(list_overlap(a, b))
