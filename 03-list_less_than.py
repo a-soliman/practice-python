f6bef1175ddd3e16df89b8b87e21d5090e239d41
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-08 10:12:35 -0700

    solved excersise 3

--- 03-list_less_than.py
+++ 03-list_less_than.py
@@ -10,3 +10,13 @@ Instead of printing the elements one by one, make a new list that has all the el
 Write this in one line of Python.
 Ask the user for a number and return a list that contains only elements from the original list a that are smaller than that number given by the user.
 '''
+
+
+def less_than(arr, num):
+    return [x for x in arr if x <= num]
+
+
+arr = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+num = 5
+
+print(less_than(arr, num))
