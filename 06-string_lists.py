3fa8b60865106aa68a1757eebdb35134bfa3b08b
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-08 10:33:14 -0700

    6

---
+++ 06-string_lists.py
@@ -0,0 +1,9 @@
+'''
+Ask the user for a string and print out whether this string is a palindrome or not. (A palindrome is a string that reads the same forwards and backwards.)
+
+Discussion
+Concepts for this week:
+
+List indexing
+Strings are lists
+'''
