f52c87e6299fffb0be10b540e97b75b7ec8f60e4
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-09 12:03:46 -0700

    soved

--- 08-guess_game.py
+++ 08-guess_game.py
@@ -6,3 +6,34 @@ Extras:
 Keep the game going until the user types “exit”
 Keep track of how many guesses the user has taken, and when the game ends, print this out.
 '''
+
+import random
+
+
+def game():
+    rand_num = random.randint(1, 10)
+    tryes = 0
+
+    while(True):
+        guess = input("guesss a number from 1 to 9: ")
+
+        if guess == "exit":
+            break
+
+        guess = int(guess)
+        tryes += 1
+
+        if (guess == rand_num):
+            print("congrats..")
+            break
+
+        if guess < rand_num:
+            print("up")
+
+        else:
+            print("down")
+
+    print(tryes)
+
+
+game()
