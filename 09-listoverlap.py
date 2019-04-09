9435dc210e3c56c4024aeeeb9c44f158398e5124
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-09 12:16:34 -0700

    solved listoverlap

--- 09-listoverlap.py
+++ 09-listoverlap.py
@@ -7,3 +7,14 @@ and write a program that returns a list that contains only the elements that are
 
 The original formulation of this exercise said to write the solution using one line of Python, but a few readers pointed out that this was impossible to do without using sets that I had not yet discussed on the blog, so you can either choose to use the original directive and read about the set command in Python 3.3, or try to implement this on your own and use at least one list comprehension in the solution.
 '''
+
+
+def overlap(l1, l2):
+    l1, l2 = list(set(l1)), list(set(l2))
+    return [x for x in l1 if x in l2]
+
+
+a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
+b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+
+print(overlap(a, b))
