5708b3c7ac15c1437233037565d9354c98ecb658
Ahmed Soliman <ahmed.soliman@programmer.net>
2019-04-09 12:20:40 -0700

    isprime

---
+++ 10-prime.py
@@ -0,0 +1,3 @@
+'''
+Ask the user for a number and determine whether the number is prime or not. (For those who have forgotten, a prime number is a number that has no divisors.). You can (and should!) use your answer to Exercise 4 to help you. Take this opportunity to practice using functions, described below.
+'''
